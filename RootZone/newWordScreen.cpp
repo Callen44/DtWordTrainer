ce604dbb02c8523a71cd2a60563d711f6a25c031
Caleb Allen <caleballen44@outlook.com>
2026-01-03 11:26:18 -0600

    After no small ammount of android code, android fully works

--- RootZone/newWordScreen.cpp
+++ RootZone/newWordScreen.cpp
@@ -8,9 +8,45 @@ newWordScreen::newWordScreen(WordSet* words, QWidget *parent)
 
     definitionBox = new QLineEdit(this);
 
+/*
+* Combo Boxes, this code simply doesn't work right on android because of a Qt bug, but the alternative, radio buttons, doesn't really feel like the right option either.
+* So, on android we use radio butons because we have to, and on other platforms we don't.
+* This first block creates either the QComboBox wortartBox, or the radio buttons.
+*/
+#ifndef __ANDROID__
+    // for all platforms other than android, combo boxes should work fine
     wortartBox = new QComboBox(this);
     wortartBox->addItems({"Verb", "Substantiv"});
     QObject::connect(wortartBox, &QComboBox::currentTextChanged, this, &newWordScreen::posChosen);
+#else
+    // please please please! fix this bug Qt (combo boxes crash due to android accessability rules
+    // add radio buttons
+    subButton = new QRadioButton("Substantiv", this);
+    subButton->setAutoExclusive(true);
+    QObject::connect(subButton, &QRadioButton::pressed, this, &newWordScreen::subChosen);
+
+    verbButton = new QRadioButton("Verb", this);
+    verbButton->setAutoExclusive(true);
+    verbButton->setChecked(true);
+    subSelectedNow = false;
+    QObject::connect(verbButton, &QRadioButton::pressed, this, &newWordScreen::verbChosen);
+
+    // hide noun specific radio buttons, but don't let them be garbage data either, and add them to their button group
+    genderBox = new QButtonGroup(this);
+
+    derButton = new QRadioButton("Der", this);
+    derButton->hide();
+    genderBox->addButton(derButton);
+    derButton->setChecked(true);
+
+    dieButton = new QRadioButton("Die", this);
+    dieButton->hide();
+    genderBox->addButton(dieButton);
+
+    dasButton = new QRadioButton("Das", this);
+    dasButton->hide();
+    genderBox->addButton(dasButton);
+#endif
 
     submitBtn = new QPushButton("Einzufügen", this);
     QObject::connect(submitBtn, &QPushButton::pressed, this, &newWordScreen::addWord);
@@ -19,12 +55,29 @@ newWordScreen::newWordScreen(WordSet* words, QWidget *parent)
     formLayout = new QFormLayout(this);
     formLayout->addRow(tr("&Deutsches Wort:"), wortBox);
     formLayout->addRow(tr("&Englische Definition:"), definitionBox);
-    formLayout->addRow(tr("&Wortart:"), wortartBox);
-    formLayout->addRow(tr(""), submitBtn);
 
+/*
+ * This next section adds either the combo box or the radio buttons to the layout QFormLayout
+*/
+#ifndef __ANDROID__
+    formLayout->addRow(tr("&Wortart:"), wortartBox);
+#else
+    formLayout->addRow(tr(""), subButton);
+    formLayout->addRow(tr(""), verbButton);
+
+    // add noun specific buttons, will be hidden until subChosen is called
+    formLayout->addRow(tr(""), derButton);
+    formLayout->addRow(tr(""), dieButton);
+    formLayout->addRow(tr(""), dasButton);
+#endif
+
+    // back to the rest, adding the submit button and completing our layout
+    formLayout->addRow(tr(""), submitBtn);
     setLayout(formLayout);
 }
 
+// Since combo boxes are so different from radio buttons, we have entirely different functions and slots to capture and process the information
+#ifndef __ANDROID__
 void newWordScreen::posChosen(QString newItem) {
     if (newItem == "Substantiv") {
         genderBox = new QComboBox(this);
@@ -53,6 +106,49 @@ void newWordScreen::addWord() {
 
     emit done();
 }
+
+#else
+
+// android specific functions and slots
+void newWordScreen::subChosen() {
+    // show all noun specific buttons
+    derButton->show();
+    dieButton->show();
+    dasButton->show();
+    subSelectedNow = true; // mark substantiv as selected
+}
+
+void newWordScreen::verbChosen() {
+    // hide all noun specific buttons
+    derButton->hide();
+    dieButton->hide();
+    dasButton->hide();
+    subSelectedNow = false; // mark verb as selected
+}
+
+void newWordScreen::addWord() {
+    // first, figure out which partOfSpeech was selected
+    if (subSelectedNow) {
+        // get our gender
+        Gender wordGender;
+        if (dasButton->isChecked()) {
+            wordGender = DAS;
+        } else if (dieButton->isChecked()) {
+            wordGender = DIE;
+        } else {
+            wordGender = DER;
+        }
+        Noun newNoun(wortBox->text(), wordGender, definitionBox->text());
+        words->addWord(newNoun);
+    } else {
+        Verb newVerb(wortBox->text(), definitionBox->text());
+        words->addWord(newVerb);
+    }
+
+    emit done();
+}
+
+#endif
 
 newWordScreen::~newWordScreen()
 {
--- RootZone/newWordScreen.h
+++ RootZone/newWordScreen.h
@@ -4,7 +4,12 @@
 #include <QLineEdit>
 #include <QComboBox>
 #include <QPushButton>
+#include <QRadioButton>
 #include "../DataZone/Wordset.h"
+
+#ifdef __ANDROID__ // android specific dependencies
+#include <QButtonGroup>
+#endif
 
 /*
 * This class is the widget that allows the user to add new words to the dictionary
@@ -23,16 +28,37 @@ signals:
 
 public slots:
     void addWord();
+#ifndef __ANDROID__ // on android, we need radio buttons instead of combo boxes, this code is android specific, see source file for more details
     void posChosen(QString newItem);
+#else
+    void subChosen();
+    void verbChosen();
+#endif
 
 private:
     WordSet* words;
 
-    // the widgets, so their data can be accessed
+    // the widgets, so their data can be accessed from other member functions of this class (I'm not a skilled enough programmer to use QObject's get child features);
     QFormLayout* formLayout;
     QLineEdit* wortBox;
     QLineEdit* definitionBox;
+    QPushButton* submitBtn;
+
+#ifndef __ANDROID__ // same as above, android specific code
     QComboBox* wortartBox;
     QComboBox* genderBox;
-    QPushButton* submitBtn;
+#else
+    QButtonGroup* wortArt; // a group for both wortArt options
+    QButtonGroup* genderBox; // a group for both gender options
+
+    // android specific buttons
+    QRadioButton* subButton;
+    QRadioButton* verbButton;
+    QRadioButton* derButton;
+    QRadioButton* dieButton;
+    QRadioButton* dasButton;
+
+    // check if sub or verb has been chose
+    bool subSelectedNow; // just keep it simple
+#endif
 };
